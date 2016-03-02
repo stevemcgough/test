53ffce13908572950ce9224b883796a827e8924f
stevemcgough <asm@locals-MBP-2.home>
2016-03-02 20:35:32 +0000

    Increase iterations
    Should make value more accurate.

--- ComputePi.java
+++ ComputePi.java
@@ -3,7 +3,7 @@ import java.util.Random;
 
 public class ComputePi {
   public static void main(String args[]) {
-    int attempts = 100000;
+    int attempts = 1000000000;
     int hits = 0;
     Random rnd = new Random();
 
